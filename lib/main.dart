a73967fc7671d6b2a97d1d3842df7d4173839d4f
Sebastian Chavez <seba.chavez21@gmail.com>
2020-10-03 22:31:10 -0300

    home ok

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      initialRoute: '/home',
+      initialRoute: '/login',
       routes: {'/login': (_) => Login(), '/home': (_) => Home()},
     );
   }
