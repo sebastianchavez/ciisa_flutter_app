ed35d2f7897b5b821a72dd015463a67488411026
sebastian.cornejo.silva@gmail.com <sebastian.cornejo.silva@gmail.com>
2020-10-05 02:31:16 -0300

    Se agrega los campos, controladores y boton de el perfil basicamente se hizo todo denuevo

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:ingenieria_flutter/widgets/button_default.dart';
+import 'package:ingenieria_flutter/widgets/text_input.dart';
 
 class Profile extends StatelessWidget {
   String first_name = "Nombre";
   String second_name = "apellido";
   String email = "Correo electronico";
   String pathImage = "assets/imgs/logo.jpg";
+  final _textController = TextEditingController();
 
   Profile(this.first_name, this.second_name, this.email);
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
+    //
+    final _controllerFirstName = TextEditingController();
+    final _controllerSecondName = TextEditingController();
+    final _controllerEmail = TextEditingController();
+    //
     final photo = Container(
-      margin: EdgeInsets.only(top: 200.0, left: 20.0),
-      width: 80.0,
-      height: 80.0,
+      //alignment: FractionalOffset.center,
+      margin: EdgeInsets.only(top: 60.0, left: 92.0, right: 90.0),
+      width: 180.0,
+      height: 180.0,
       decoration: BoxDecoration(
           shape: BoxShape.circle,
           image: DecorationImage(image: AssetImage(pathImage))),
@@ -49,15 +59,145 @@ class Profile extends StatelessWidget {
       ),
     );
 
-    return Stack(
+    final boxEmail2 = ListView(
       children: <Widget>[
-        Container(
-          color: Colors.white,
-        ),
         Column(
-          children: [photo, boxFirstName, boxSecondName, boxEmail],
-        )
+          children: <Widget>[
+            TextField(
+              maxLengthEnforced: false,
+              maxLines: null,
+              controller: _textController,
+              decoration: new InputDecoration(hintText: "Add info 1"),
+            ),
+            TextField(
+              maxLengthEnforced: false,
+              maxLines: null,
+              controller: _textController,
+              decoration: new InputDecoration(hintText: "Add info 1"),
+            ),
+          ],
+        ),
       ],
+    );
+
+    final forum = Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: "Email",
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return "Porfavor entra un email";
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: RaisedButton(
+              onPressed: () {
+                if (_formKey.currentState.validate()) {
+                  //process data
+                }
+              },
+              child: Text("Submit"),
+            ),
+          )
+        ],
+      ),
+    );
+
+    final emailBox = Container(
+      margin: EdgeInsets.all(20.0),
+      padding: EdgeInsets.only(top: 220.0),
+      child: ListView(
+        children: <Widget>[
+          //Container(),//foto
+          Container(
+            margin: EdgeInsets.only(bottom: 20.0),
+            child: TextInput(
+              hintText: "Nombre",
+              inputType: null,
+              maxLines: 1,
+              controller: _controllerFirstName,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(bottom: 20.0),
+            child: TextInput(
+              hintText: "Apellido",
+              inputType: null,
+              maxLines: 1,
+              controller: _controllerSecondName,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(bottom: 20.0),
+            child: TextInput(
+              hintText: "Email",
+              inputType: TextInputType.emailAddress,
+              maxLines: 1,
+              controller: _controllerEmail,
+            ),
+          ),
+          ButtonDefault(
+            text: 'Guardar',
+            onPressed: () {
+              Navigator.pushReplacementNamed(context, '/home');
+            },
+          )
+        ],
+      ),
+    );
+
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          /*
+          Row(
+            children: <Widget>[
+              Flexible(
+                  child: Container(
+                padding: EdgeInsets.only(top: 45.0, left: 20.0, right: 10.0),
+              ))
+              //
+            ],
+            //
+          ),*/
+          photo,
+          emailBox,
+
+          /*
+        Column(
+          children: [emailBox],
+        ),*/
+          /*
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          //mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [photo, emailBox],
+        ),*/
+          /*
+        SingleChildScrollView(
+          child: Container(
+            alignment: Alignment.topCenter,
+            padding: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * .35,
+                right: 20.0,
+                left: 20.0),
+            child: Column(
+              children: <Widget>[emailBox],
+            ),
+          ),
+        )
+        */
+        ],
+      ),
     );
   }
 }
